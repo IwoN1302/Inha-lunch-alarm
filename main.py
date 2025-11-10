852309b0bcf4c381e4b669a4de65d78faa3df583
Taewon Min <mtw1203@gmail.com>
2025-11-10 10:52:44 +0900

    api data importing
    It's really tough, I need to learn more about header, which is telling a lie that "I'm a browser"

--- main.py
+++ main.py
@@ -12,8 +12,14 @@ url = "https://www.inhatc.ac.kr/kr/485/subview.do"
 response = http.request("GET", url)
 
 html = response.data.decode("utf-8", errors="ignore")
-print(html)
+# print(html)
 
 soup = BeautifulSoup(html, "html.parser")
+tables = soup.find("tbody")
+rows = tables.find_all('tr')
 
-# print(soup)
+print("="*50)
+    
+print(tables)
+print("="*50)
+print(rows)
